9615a43265ee4597693a5aad34bb16d6437e4dd4
Mukul <mukulkundu22@gmail.com>
2024-07-21 00:01:31 +0530

    Nav Menu Updation

--- Boons/script.js
+++ Boons/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const closebtn = document.querySelectorAll(".close-btn");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -11,5 +12,7 @@ navLink.forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    closebtn.classList.remove("active");
   })
 );
+
