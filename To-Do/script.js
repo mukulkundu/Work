be097e9323f32b042110daea3afaecf36e644eb6
Mukul <mukulkundu22@gmail.com>
2024-07-25 00:29:15 +0530

    Almost Completed

--- To-Do/script.js
+++ To-Do/script.js
@@ -1,70 +1,110 @@
 let data = [];
+let originalData = [];
 
+let sortButton = document.getElementById("move-down-toggler");
 let taskList = document.querySelector(".tasks");
-
 const button = document.getElementById("additem");
 
 button.addEventListener("click", () => {
-    addItem();
-    clearTextbox();
+  addItem();
+  clearTextbox();
 });
 
 function addItem() {
   const currentdata = document.getElementById("todo-text").value;
-  
-  if(currentdata==""){
+
+  if (currentdata == "") {
     alert("Enter Some Task");
-  }
-  else{
+  } else {
     data.push({
       title: currentdata,
-      state: 'pending'
+      state: '0'
     });
+    originalData.push({
+      title: currentdata,
+      state: '0'
+    });
+
     let taskItem = createListItem(currentdata);
     let li = document.createElement('li');
-    li.classList.add('taskitem')
-    li.id = "li-"+currentdata;
-    li.innerHTML=taskItem;
+    li.classList.add('taskitem');
+    li.id = "li-" + currentdata;
+    li.innerHTML = taskItem;
 
     taskList.appendChild(li);
   }
 }
 
-function clearTextbox(){
+function clearTextbox() {
   let taskInputField = document.getElementById("todo-text");
-
   taskInputField.value = "";
 }
 
 function checkBoxChange(event) {
-    if(event.checked) {
-        // our todo is done
-        let dataItem = data.find(d => d.title == event.id);           dataItem.state = 'done';
-      
-        let title = document.getElementById('title-'+event.id);
-
-      title.style.textDecoration="line-through";
-        
-    } else {
-              // our todo is pending
-        let dataItem = data.find(d => d.title == event.id);           dataItem.state = 'pending';
-      
-        let title = document.getElementById('title-'+event.id);
-
-      title.style.textDecoration="none";
-    }
+  let dataItem = data.find(d => d.title == event.id);
+  if (event.checked) {
+    dataItem.state = '1';
+    let title = document.getElementById('title-' + event.id);
+    title.style.textDecoration = "line-through";
+  } else {
+    dataItem.state = '0';
+    let title = document.getElementById('title-' + event.id);
+    title.style.textDecoration = "none";
+  }
 }
 
 function deleteItem(event) {
-  let li = document.getElementById("li-"+event.id);
+  let li = document.getElementById("li-" + event.id);
   li.remove();
+  data = data.filter(d => d.title !== event.id);
+  originalData = originalData.filter(d => d.title !== event.id);
 }
 
-function createListItem(taskName) {
-  return `<span id="title-${taskName}" >${taskName}</span> <div class="buttons">
-                            <input class="checkbox" type="checkbox" value="" onChange="checkBoxChange(this)" class="task-checkbox"  id="${taskName}">
-                            <label></label>
-                            <img id="${taskName}" src="/Aeroqube/To-Do/delete.png" class="delete" onclick="deleteItem(this)">
-                        </div>`
-} 
+function sortItems(event) {
+  if (event.checked) {
+    data = data.sort(comparator);
+    renderList();
+  } else {
+    data = [...originalData];
+    renderList2();
+  }
+  
+}
 
+function comparator(a, b) {
+  return a.state - b.state;
+}
+
+function createListItem(taskName, checked = false) {
+  return `<span style="${checked ? 'text-decoration: line-through' : ''}" id="title-${taskName}">${taskName}</span> 
+          <div class="buttons">
+            <input class="checkbox" ${checked ? 'checked' : ''} type="checkbox" onChange="checkBoxChange(this)" class="task-checkbox" id="${taskName}">
+            <label></label>
+            <img id="${taskName}" src="/Aeroqube/To-Do/delete.png" class="delete" onclick="deleteItem(this)">
+          </div>`;
+}
+
+function renderList() {
+  taskList.innerHTML = '';
+  data.forEach(item => {
+    let taskItem = createListItem(item.title, item.state === '1');
+    let li = document.createElement('li');
+    li.classList.add('taskitem');
+    li.id = "li-" + item.title;
+    li.innerHTML = taskItem;
+    taskList.appendChild(li);
+  });
+}
+
+
+function renderList2() {
+  taskList.innerHTML = '';
+  data.forEach(item => {
+    let taskItem = createListItem(item.title, item.state === '1');
+    let li = document.createElement('li');
+    li.classList.add('taskitem');
+    li.id = "li-" + item.title;
+    li.innerHTML = taskItem;
+    taskList.appendChild(li);
+  });
+}
