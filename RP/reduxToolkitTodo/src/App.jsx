d30c762f7d2d24209e911867d91b6429a73ff208
Mukul <mukulkundu22@gmail.com>
2025-04-12 03:06:29 +0530

    Redux Toolkit Todo

---
+++ RP/reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Todos from './components/AddTodo'
+import AddTodo from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
