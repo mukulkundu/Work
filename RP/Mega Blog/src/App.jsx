9dc0175ce8836b08e11e7a41492ff4c26b3c2075
Mukul <mukulkundu22@gmail.com>
2025-04-15 02:58:45 +0530

    Components made before login

--- RP/Mega Blog/src/App.jsx
+++ RP/Mega Blog/src/App.jsx
@@ -1,13 +1,46 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-    <h1>A blog app with appwrite</h1>
-    </>
-  )
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
