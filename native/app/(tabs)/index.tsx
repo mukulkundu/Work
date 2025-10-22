51616fd58986ef091ce7737d242c1ad8d219a513
Mukul <mukulkundu22@gmail.com>
2025-10-22 21:01:08 +0530

    React Native add

---
+++ native/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+
+const app = () => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
