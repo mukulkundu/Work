33c1b4490bc448cc7fc0ac56641b380a6ab15891
Mukul <mukulkundu22@gmail.com>
2026-01-17 01:04:28 +0530

    backend

---
+++ BP/FullStack/backend/server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+
+const app = express();
+
+const jokes = [
+    {
+        id: 1,
+        title: "A joke",
+        content: "This is a joke"
+    },
+    {
+        id: 2,
+        title: "Another joke",
+        content: "This is another joke"
+    },
+    {
+        id: 3,
+        title: "Third joke",
+        content: "This is the third joke"
+    },
+    {
+        id: 4,
+        title: "Fourth joke",
+        content: "This is the fourth joke"
+    },
+    {
+        id: 5,
+        title: "Fifth joke",
+        content: "This is the fifth joke"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send("Server is ready");
+});
+
+app.get('/api/jokes', (req, res) => {
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Served at port number ${port}`);
+    
+});
